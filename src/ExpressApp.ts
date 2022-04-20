aec52a476eff5227547980da9da7817c768313b0
Eduardo Aquino <edutraquino@gmail.com>
2022-04-19 21:05:47 -0300

    using app.render to render views

--- src/ExpressApp.ts
+++ src/ExpressApp.ts
@@ -6,7 +6,7 @@ import { FindAllController } from "./input/controllers/FindAllController";
 import { InMemoryRepository } from "./output/repositories/test/InMemoryRepository";
 
 const app = Express();
-const repository = new InMemoryRepository<Transaction>();
+const transactionRepository = new InMemoryRepository<Transaction>();
 
 // Set view engine to ejs
 app.set("view engine", "ejs");
@@ -15,11 +15,25 @@ app.set("view engine", "ejs");
 app.use(Express.static("public"));
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
-
 // Configure multer middleware and add to route
 const upload = multer({ dest: "uploads/" });
 
-app.get("/transactions", async (req, res) => new FindAllController(repository).handle(req, res));
-app.post("/submit", upload.single("files"), async (req, res) => await new FileInputController(repository).handle(req, res));
+// Static routes
+app.get("/", (req, res) => {
+    res.render("index", { user: "No User" })
+})
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+app.get("/users", (req, res) => {
+    res.render("users")
+})
+
+// API Routes
+app.get("/transactions", async (req, res) => new FindAllController(transactionRepository).handle(req, res));
+app.post("/submit", upload.single("files"), async (req, res) => await new FileInputController(transactionRepository).handle(req, res));
 
 export { app };
