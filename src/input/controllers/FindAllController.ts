1267e8b63c2e8917ebc8a0ac8c1cbbca69a2ad1e
Eduardo Aquino <edutraquino@gmail.com>
2022-04-19 22:00:35 -0300

    create/using use case

--- src/input/controllers/FindAllController.ts
+++ src/input/controllers/FindAllController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { FindAllFromUserUseCase } from "../../application/useCases/FindAllFromUserUseCase";
 import IRepository from "../../output/repositories/IRepository";
 
 export class FindAllController {
@@ -7,7 +8,7 @@ export class FindAllController {
 
     async handle(request: Request, response: Response) {
 
-        const results = await this.repository.find();
+        const results = await new FindAllFromUserUseCase(this.repository).execute(request.params.id);
         return response.json(results);
     }
 }
