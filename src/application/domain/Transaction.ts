69e1e5610d861b69fca74fa225963867bd0baf61
Eduardo Aquino <edutraquino@gmail.com>
2022-04-21 11:40:44 -0300

    refact: move parse line-transact fn to csv adapter

--- src/application/domain/Transaction.ts
+++ src/application/domain/Transaction.ts
@@ -25,28 +25,4 @@ export class Transaction {
     static create(props: TransactionProps, id?: string) {
         return new Transaction(props, id);
     }
-
-    static createFromStringArray(source: string[]) {
-
-        const props = {
-            originBankName: source[0],
-            originBankAgency: source[1],
-            originBankNumber: source[2],
-            destinyBankName: source[3],
-            destinyBankAgency: source[4],
-            destinyBankNumber: source[5],
-            value: source[6],
-            date: new Date(source[7]),
-            allFieldsFull: true
-        };
-
-        const entries = Object.entries(props);
-        const hasNullField = entries.some(el => {
-            const [_, value] = el;
-            return (value == null || value == undefined || value == "")
-        })
-        props.allFieldsFull = !hasNullField;
-
-        return new Transaction(props);
-    }
 }
--- src/input/adapters/CSVToTransactionAdapter.ts
+++ src/input/adapters/CSVToTransactionAdapter.ts
@@ -8,15 +8,44 @@ export class CSVToTransactionAdapter {
     execute(userId: string) {
         const data = getLinesAndColumnsFromCSV(this.source);
 
-        const firstEl = Transaction.createFromStringArray(data[0]);
+        const allTransactions = data.map(line => {
+            return this.createTransactionFromLine(line, userId)
+        })
+
+        const firstTransaction = allTransactions[0];
 
         // Criando um array de transações
         // Filtrando apenas transações da mesma data
         // E transações sem campos nulos
-        const transactions = data.map(Transaction.createFromStringArray)
-            .filter(el => el.props.date.getDate() == firstEl.props.date.getDate()
+        const filtredTransactions = allTransactions
+            .filter(el => el.props.date.getDate() == firstTransaction.props.date.getDate()
                 && el.props.allFieldsFull
             );
-        return transactions;
+        return filtredTransactions;
+    }
+
+    private createTransactionFromLine(source: string[], userId: string) {
+
+        const props = {
+            originBankName: source[0],
+            originBankAgency: source[1],
+            originBankNumber: source[2],
+            destinyBankName: source[3],
+            destinyBankAgency: source[4],
+            destinyBankNumber: source[5],
+            value: source[6],
+            date: new Date(source[7]),
+            allFieldsFull: true,
+            userId
+        };
+
+        const entries = Object.entries(props);
+        const hasNullField = entries.some(el => {
+            const [_, value] = el;
+            return (value == null || value == undefined || value == "")
+        })
+        props.allFieldsFull = !hasNullField;
+
+        return Transaction.create(props);
     }
 }
