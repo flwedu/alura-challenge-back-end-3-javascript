0015f570b9f1e0ad7b10ba33d4305ae42986f8fa
Eduardo Aquino <edutraquino@gmail.com>
2022-04-20 22:22:44 -0300

    refact: change decrypt method

--- src/util/password-encrypt.ts
+++ src/util/password-encrypt.ts
@@ -4,7 +4,7 @@ async function hashPassword(password: string): Promise<string> {
     return new Promise((resolve, reject) => {
 
         // Generating salt
-        const salt = crypto.randomBytes(16).toString("hex");
+        const salt = crypto.randomBytes(8).toString("hex");
 
         crypto.scrypt(password, salt, 64, (err, derivedKey) => {
             // Error creating hash
@@ -19,12 +19,11 @@ async function checkPassword(password: string, hash: string): Promise<boolean> {
 
     return new Promise((resolve, reject) => {
         const [salt, key] = hash.split(":");
-        const keyBuffer = Buffer.from(key, "hex");
 
         crypto.scrypt(password, salt, 64, (err, derivedKey) => {
             if (err) reject(err);
-            resolve(crypto.timingSafeEqual(keyBuffer, derivedKey));
-        })
+            resolve(key == derivedKey.toString("hex"));
+        });
     })
 }
 
