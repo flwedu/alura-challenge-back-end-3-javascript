97767a107faf11bc16c998e405f97e477a98c2f9
Eduardo Aquino <edutraquino@gmail.com>
2022-04-24 20:58:35 -0300

    style: ident code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -29,18 +29,28 @@ const configureRouter = (repositories: RepositoriesSource, encryptor: IEncryptor
     router.use("/users", (req, res, next) => verifyCredentials.handle(req, res, next));
 
     // Static routes
-    router.get("/", (req, res) => res.redirect("/home"));
-    router.get("/home", (req, res) => new HomeViewController(transactionRepository).handle(req, res));
-    router.get("/home/:id", (req, res) => new TransactionImportDetailsViewController(transactionRepository, userRepository).handle(req, res))
-    router.get("/login", (req, res) => new LoginUserViewController(userRepository).handle(req, res));
-    router.get("/register", (req, res) => new RegisterUserViewController(userRepository).handle(req, res));
-    router.get("/users", (req, res) => new UsersViewController(userRepository).handle(req, res));
-    router.get("/logout", (req, res) => new LogoutUserController().handle(req, res));
+    router.get("/",
+        (req, res) => res.redirect("/home"));
+    router.get("/home",
+        (req, res) => new HomeViewController(transactionRepository).handle(req, res));
+    router.get("/home/:id",
+        (req, res) => new TransactionImportDetailsViewController(transactionRepository, userRepository).handle(req, res))
+    router.get("/login",
+        (req, res) => new LoginUserViewController(userRepository).handle(req, res));
+    router.get("/register",
+        (req, res) => new RegisterUserViewController(userRepository).handle(req, res));
+    router.get("/users",
+        (req, res) => new UsersViewController(userRepository).handle(req, res));
+    router.get("/logout",
+        (req, res) => new LogoutUserController().handle(req, res));
 
     // POST Routes
-    router.post("/home", upload.single("files"), (req, res) => new FileInputController(transactionRepository).handle(req, res));
-    router.post("/register", (req, res) => new RegisterUserController(userRepository, encryptor).handle(req, res));
-    router.post("/login", (req, res) => new LoginUserController(userRepository, encryptor).handle(req, res));
+    router.post("/home", upload.single("files"),
+        (req, res) => new FileInputController(transactionRepository).handle(req, res));
+    router.post("/register",
+        (req, res) => new RegisterUserController(userRepository, encryptor).handle(req, res));
+    router.post("/login",
+        (req, res) => new LoginUserController(userRepository, encryptor).handle(req, res));
 
     return router;
 }
