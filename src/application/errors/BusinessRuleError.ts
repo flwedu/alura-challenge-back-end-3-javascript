297e10725e74e9095a1e9e1ea2d6c2e7b51d501b
Eduardo Aquino <edutraquino@gmail.com>
2022-04-25 19:29:48 -0300

    feat: create ErrorMessage and Errors classes

---
+++ src/application/errors/BusinessRuleError.ts
@@ -0,0 +1,7 @@
+export default class BusinessRuleError extends Error {
+
+    constructor(message: string) {
+        super(message);
+        this.name = "BusinessRuleError";
+    }
+}
---
+++ src/application/errors/ResourceNotFoundError.ts
@@ -0,0 +1,10 @@
+import { ErrorMessage } from "./ErrorMessage";
+
+export default class ResourceNotFoundError extends Error {
+
+    constructor(id?: string) {
+        const message = id ? ErrorMessage.RESOURCE_NOT_FOUND(id) : "";
+        super(message);
+        this.name = "ResourceNotFoundError";
+    }
+}
