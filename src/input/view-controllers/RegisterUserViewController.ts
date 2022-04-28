7426aab0dcad8fc50a402eca567c668ef7a746ec
Eduardo Aquino <edutraquino@gmail.com>
2022-04-28 19:28:56 -0300

    refact: remove useless dependency

--- src/input/view-controllers/RegisterUserViewController.ts
+++ src/input/view-controllers/RegisterUserViewController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express";
-import { User } from "../../application/domain/User";
-import IRepository from "../../output/repositories/IRepository";
 
 export class RegisterUserViewController {
 
-    constructor(private readonly repository: IRepository<User>) { };
+    constructor() { };
 
     async handle(request: Request, response: Response) {
 
