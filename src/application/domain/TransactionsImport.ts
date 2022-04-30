002c084a7e8bd33a0c998dce6feef9d4485e7b8f
Eduardo Aquino <edutraquino@gmail.com>
2022-04-30 11:32:34 -0300

    refact: remove transactions[] from class

--- src/application/domain/TransactionsImport.ts
+++ src/application/domain/TransactionsImport.ts
@@ -1,11 +1,8 @@
 import crypto from "crypto";
-import { Transaction } from "./Transaction"
-import { User } from "./User"
 
 export type TransactionsImportProps = {
     transactionsDate: Date
     userId: string
-    transactions: Transaction[]
 }
 
 export class TransactionsImport {
@@ -14,14 +11,12 @@ export class TransactionsImport {
     public date: Date
     public transactionsDate: Date
     public userId: string
-    public transactions: Transaction[]
 
     private constructor(props: TransactionsImportProps) {
         this.id = crypto.randomUUID();
         this.date = new Date();
         this.transactionsDate = props.transactionsDate;
         this.userId = props.userId;
-        this.transactions = props.transactions;
     };
 
     static create(props: TransactionsImportProps) {
