287af3d1d0a26bf5af5a9466ae11f2dbc7fb534e
Eduardo Aquino <edutraquino@gmail.com>
2022-04-30 11:35:16 -0300

    refact: get transactions

--- src/input/view-controllers/TransactionImportDetailsViewController.ts
+++ src/input/view-controllers/TransactionImportDetailsViewController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { LoadAllTransactionsByImportIdUseCase } from "../../application/useCases/transactions/load-all-transactions-by-import-id-use-case";
 import { LoadTransactionImportByIdUseCase } from "../../application/useCases/transactions/LoadTransactionImportByIdUseCase";
 import { LoadUserByIdUseCase } from "../../application/useCases/user/LoadUserByIdUseCase";
 import { RepositoriesSource } from "../../output/repositories/RepositoriesSource";
@@ -12,8 +13,9 @@ export class TransactionImportDetailsViewController {
         const id = request.params.id;
         try {
             const transactionsImport = await new LoadTransactionImportByIdUseCase(this.repositories.transactionsImports).execute(id);
+            const transactions = await new LoadAllTransactionsByImportIdUseCase(this.repositories.transactions).execute(id)
             const user = await new LoadUserByIdUseCase(this.repositories.users).execute(transactionsImport.userId);
-            return response.render("details", { transactionsImport, user });
+            return response.render("details", { transactionsImport, user, transactions });
         } catch (err) {
             return response.redirect("/error");
         }
