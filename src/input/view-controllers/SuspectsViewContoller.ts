30c2d5c71aaedb571b8b3a069e90e23fa255361b
Eduardo Aquino <edutraquino@gmail.com>
2022-05-02 10:51:25 -0300

    feat: suspects view. Loads empty array as default

--- src/input/view-controllers/SuspectsViewContoller.ts
+++ src/input/view-controllers/SuspectsViewContoller.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from "express";
-import { RepositoriesSource } from "../../output/repositories/RepositoriesSource";
 
 export class SuspectsViewController {
 
-    constructor(private readonly repositories: RepositoriesSource) { };
+    constructor() { };
 
     async handle(request: Request, response: Response) {
 
-        const transactions = await this.repositories.transactions.findAll();
-        return response.render("suspects", { transactions });
+        return response.render("suspects", { transactions: [], agencies: [], accounts: [] });
     }
 }
