74b2f70c70782dc4e5e5e1292af61f7c0b12ac74
Eduardo Aquino <edutraquino@gmail.com>
2022-05-02 10:46:16 -0300

    chore: jest.config, tsconfig and module-alias

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const { resolve } = require("path");
+const root = resolve(__dirname);
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  rootDir: root,
+  displayName: "src-tests",
+  testMatch: ["<rootDir>/src/**/*.spec.ts"],
+  testEnvironment: "node",
+  clearMocks: true,
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "@src/(.*)": "<rootDir>/src/$1",
+    "@test/(.*)": "<rootDir>/test/$1",
+  },
 };
---
+++ tests/jest.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require("path");
+const root = resolve(__dirname, "..");
+const rootConfig = require(`${root}/jest.config.js`);
+
+module.exports = {
+  ...rootConfig,
+  ...{
+    rootDir: root,
+    displayName: "e2e-tests",
+    setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
+    testMatch: ["<rootDir>/test/**/*.spec.ts"],
+  },
+};
